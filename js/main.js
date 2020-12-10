db680b8b25d093871b6e072e6288d13db2eeacc9
keisukeashitomi <75768447+keisukeashitomi@users.noreply.github.com>
2020-12-11 07:57:35 +0900

    html,css,js 微調整

--- js/main.js
+++ js/main.js
@@ -39,33 +39,33 @@
 
   $(function() {
     $('.shopinfo').each(function() {
-      // スライド（画像）の数を取得
+     
       var $slides = $(this).find('img'),
           slideNum = $slides.length,
-          currentIdx = 0; // 何番目か
+          currentIdx = 0;
     
-      // 最初の画像をフェードイン
+    
       $(".shopinfo img").eq(currentIdx).fadeIn();
     
-      // 3秒後に次のスライドを表示
+    
       setTimeout(dispNextSlide, 2000);
     
-      // 次のスライドを表示するメソッド
+      
       function dispNextSlide() {
         var nextIdx = currentIdx + 1;
     
-        // 最後のスライドの場合ははじめに戻る
+      
         if (nextIdx > (slideNum - 1)) {
           nextIdx = 0;
         }
     
-        // 現在のスライドをフェードアウト
+       
         $(".shopinfo img").eq(currentIdx).fadeOut();
     
-        // 次のスライドをフェードイン
+        
         $(".shopinfo img").eq(nextIdx).fadeIn();
     
-        // インデックスを更新
+       
         currentIdx = nextIdx;
       }
     })
